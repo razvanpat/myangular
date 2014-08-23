de4c5bf4e3d1a80492b71ecdc05e23175956eb01
Razvan Pat <razvan.pat@gmail.com>
2014-08-23 13:45:01 +0300

    pg 21

---
+++ src/scope.js
@@ -0,0 +1,49 @@
+/* jshint globalstrict: true */
+'use strict';
+
+function Scope() {
+	this.$$watchers = [];
+	this.$$lastDirtyWatch = null;
+}
+
+function initWatchVal() {}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+	var watcher = {
+		watchFn: watchFn,
+		listenerFn: listenerFn || function() {},
+		last: initWatchVal
+	};
+	this.$$watchers.push(watcher);
+	this.$$lastDirtyWatch = null;
+};
+
+Scope.prototype.$$digestOnce = function() {
+	var self = this;
+	var newValue, oldValue, dirty = false;
+	_.forEach(this.$$watchers, function(watcher) {
+		newValue = watcher.watchFn(self);
+		oldValue = watcher.last;
+		if (newValue !== oldValue) {
+			self.$$lastDirtyWatch = watcher;
+			watcher.last = newValue;
+			watcher.listenerFn(newValue,
+				(oldValue === initWatchVal ? newValue : oldValue),
+				self);
+			dirty = true;
+		} else if (self.$$lastDirtyWatch === watcher) {
+			return false;
+		}
+	});
+	return dirty;
+};
+
+Scope.prototype.$digest = function() {
+	var ttl = 10;
+	this.$$lastDirtyWatch = null;
+	while(this.$$digestOnce()) {
+		if(!(ttl--)) {
+			throw "10 digest iterations reached";
+		}
+	}
+};
